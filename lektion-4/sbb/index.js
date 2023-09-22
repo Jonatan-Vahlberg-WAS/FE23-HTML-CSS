bdd653f55187d414c2879aec0a6585b11265f630
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-09-22 11:12:26 +0200

    add basic javascript

---
+++ lektion-4/sbb/index.js
@@ -0,0 +1,18 @@
+
+const headerFoldOutMenu = document.querySelector(".nav-list-fold-out-menu")
+
+const menuButton = document.querySelector(".nav-list-menu")
+const onClick = (event) => {
+    event.target.classList.toggle("nav-list-menu--open")
+    headerFoldOutMenu.classList.toggle("nav-list-fold-out-menu--open")
+    //set text to "x" if menu is open, otherwise set it to "☰"
+    console.log(event.target, event.target.innerText)
+    if (event.target.classList.contains("nav-list-menu--open")) {
+        event.target.innerText = "x"
+    }
+    else {
+        event.target.innerText = "☰"
+    }
+    
+}
+menuButton.addEventListener("click", onClick)
